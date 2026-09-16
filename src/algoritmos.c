55b95bfb59fcdbad21f593b27fbc85620cec6802
mathbmach <matheusmachado@usp.br>
2026-09-16 00:26:16 -0300

    Busca binária recursiva implementada

--- src/algoritmos.c
+++ src/algoritmos.c
@@ -12,11 +12,27 @@ int busca_binaria_iterativa(int *v, int n, int valor) {
     return 0;
 }
 
-int busca_binaria_recursiva(
+void busca_binaria_recursiva(
     int *v,
     int inicio,
     int fim,
-    int valor
+    int p
 ) {
-    return 0;
+    if (inicio > fim) {
+        printf("NAO\n");
+        return;
+    }
+
+    int meio = inicio + (fim-inicio) / 2;
+
+    if (v[meio] == p) {
+        printf("SIM\n");
+        return;
+    }
+    
+    if (v[meio] < p) 
+        busca_binaria_recursiva(v, meio + 1, fim, p);
+    
+    else
+        busca_binaria_recursiva(v, inicio, meio - 1, p);
 }
